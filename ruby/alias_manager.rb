08c8063b91029cc6041848ff32b767210953ec01
nbushell4977 <nbushell4977@gmail.com>
2016-04-01 18:10:28 -0700

    alias manager

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,3 +1,13 @@
+#use .reverse to switch the first and last name,
+.#downcase the whole string,
+#isolate the vowels using .select,
+#use index to move vowels ahead in "aeiou" string,
+#create interface,
+#print array,
+#decode array
+
+
+
 array = Array.new
 valid_input = false
 until valid_input
