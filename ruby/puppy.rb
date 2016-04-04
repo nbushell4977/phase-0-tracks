2aca8dbeb5dd105bd66128f02b205ba410cc9a8a
nbushell4977 <nbushell4977@gmail.com>
2016-04-04 12:38:39 -0700

    create puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,12 @@
+#class Puppy
+#end
+#Puppy.methods
+#duchess = Puppy.new
+#fido = Puppy.new
+#spot = Puppy.new
+#spot.class
+#duchess == fido
+#fido.instance_of?(Array)
+#fido.instance_of?(Puppy)
+#spot.play_dead
+
